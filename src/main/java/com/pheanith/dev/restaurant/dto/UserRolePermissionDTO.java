4a774987ad7b813ba6d14901e504c068c66095ec
unknown <pheanithroeun1@gmail.com>
2025-07-11 23:21:12 +0700

    create to store user role and permissions fetched from db

--- src/main/java/com/pheanith/dev/restaurant/dto/UserRolePermissionDTO.java
+++ src/main/java/com/pheanith/dev/restaurant/dto/UserRolePermissionDTO.java
@@ -2,14 +2,11 @@ package com.pheanith.dev.restaurant.dto;
 
 import java.util.Set;
 
-import com.pheanith.dev.restaurant.entity.Permission;
-import com.pheanith.dev.restaurant.entity.Role;
-
 import lombok.Data;
 
 @Data
 public class UserRolePermissionDTO {
 	private String username;
-	private Set<Permission> permissions;
-	private Set<Role> roles;
+	private Set<String> permissions;
+	private Set<String> roles;
 }
