b0326d6e1d017eb7b4821ffa54579265a494192b
unknown <pheanithroeun1@gmail.com>
2025-07-13 10:25:03 +0700

    add jpa specification.

--- src/main/java/com/pheanith/dev/restaurant/repository/UserRepository.java
+++ src/main/java/com/pheanith/dev/restaurant/repository/UserRepository.java
@@ -3,12 +3,13 @@ package com.pheanith.dev.restaurant.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import com.pheanith.dev.restaurant.entity.User;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
 	Optional<User> findByUsername(String username);
 	boolean existsByUsername(String username);
 	boolean existsByEmail(String email);
