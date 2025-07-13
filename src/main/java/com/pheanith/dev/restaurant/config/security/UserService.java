55aac4a0a29da2e48cc46206d30a6309ffebed19
unknown <pheanithroeun1@gmail.com>
2025-07-13 10:10:07 +0700

    add retrieve and delete user method.

--- src/main/java/com/pheanith/dev/restaurant/config/security/UserService.java
+++ src/main/java/com/pheanith/dev/restaurant/config/security/UserService.java
@@ -1,11 +1,18 @@
 package com.pheanith.dev.restaurant.config.security;
 
+import java.util.Map;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+
 import com.pheanith.dev.restaurant.dto.UserRolePermissionDTO;
+import com.pheanith.dev.restaurant.entity.User;
 
 public interface UserService {
 
 	Optional<AuthUser> findUserByUsername(String username);
 	UserRolePermissionDTO getUserRolesAndPermissions(String username);
+	Page<?> userFilter(Map<String, String> params);
+	User getUserById(Long id);
+	void deleteUser(Long id);
 }
