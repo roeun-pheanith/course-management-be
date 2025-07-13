de986e35a91323c5c7b5dec2bd62b2cd84be992f
unknown <pheanithroeun1@gmail.com>
2025-07-13 10:17:43 +0700

    add retrieve by page, retrieve by id and delete by id

--- src/main/java/com/pheanith/dev/restaurant/controller/UserController.java
+++ src/main/java/com/pheanith/dev/restaurant/controller/UserController.java
@@ -1,14 +1,19 @@
 package com.pheanith.dev.restaurant.controller;
 
 import java.security.Principal;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pheanith.dev.restaurant.config.security.UserService;
+import com.pheanith.dev.restaurant.dto.PageDTO;
 import com.pheanith.dev.restaurant.dto.UserRolePermissionDTO;
 
 @RestController
@@ -18,11 +23,31 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 	
+	@GetMapping
+	public ResponseEntity<?> getUserByPage(Map<String, String> params){
+		Page<?> userFilter = userService.userFilter(params);
+		PageDTO pageDTO = new PageDTO(userFilter);
+		return ResponseEntity.ok(pageDTO);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<?> getUserById(@PathVariable Long id){
+		return ResponseEntity.ok(userService.getUserById(id));
+	}
+	
 	@GetMapping("/roles-permissions")
 	public ResponseEntity<?> getRoleAndPermission(Principal principal){
 		UserRolePermissionDTO userRolesAndPermissions = userService.getUserRolesAndPermissions(principal.getName());
 		System.out.println("this is roles and permissions: %s ".formatted(userRolesAndPermissions));
 		return ResponseEntity.ok(userRolesAndPermissions);
 	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> deleteUser(@PathVariable Long id) {
+		userService.deleteUser(id);
+		return ResponseEntity.ok().build();
+	}
+	
+	
 
 }
