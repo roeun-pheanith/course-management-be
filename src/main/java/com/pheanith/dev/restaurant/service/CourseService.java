413e633934f28b84e1fe5557daf500ae0a825124
unknown <pheanithroeun1@gmail.com>
2025-07-14 23:01:37 +0700

    add/remove any methods

--- src/main/java/com/pheanith/dev/restaurant/service/CourseService.java
+++ src/main/java/com/pheanith/dev/restaurant/service/CourseService.java
@@ -1,27 +1,30 @@
 package com.pheanith.dev.restaurant.service;
 
-import com.pheanith.dev.restaurant.dto.CourseRequestDTO;
-import com.pheanith.dev.restaurant.dto.CourseResponseDTO;
-import com.pheanith.dev.restaurant.entity.Course;
-import org.springframework.data.domain.Page;
-import org.springframework.web.multipart.MultipartFile;
-
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.data.domain.Page;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.pheanith.dev.restaurant.dto.CourseAccessStatusDTO;
+import com.pheanith.dev.restaurant.dto.CourseRequestDTO;
+import com.pheanith.dev.restaurant.dto.CourseResponseDTO;
+import com.pheanith.dev.restaurant.entity.Course;
+
 public interface CourseService {
-    // Renamed for better semantics (createWithFiles)
-    CourseResponseDTO createCourseWithFiles(CourseRequestDTO courseDTO, MultipartFile thumbnailFile, MultipartFile videoFile) throws IOException;
+	CourseResponseDTO createCourseWithFiles(CourseRequestDTO courseDTO, MultipartFile thumbnailFile, MultipartFile videoFile) throws IOException;
 
     Course getById(Long id);
 
-    // Renamed for better semantics (updateWithFiles)
     CourseResponseDTO updateCourseWithFiles(Long id, CourseRequestDTO courseDTO, MultipartFile thumbnailFile, MultipartFile videoFile) throws IOException;
 
     void delete(Long courseId);
 
     Page<CourseResponseDTO> courseFilter(Map<String, String> params);
 
-    List<CourseResponseDTO> getAllCourses(); // If you still need this for a non-paginated list
+    List<CourseResponseDTO> getAllCourses();
+    
+    // This is the only method for public access status
+    CourseAccessStatusDTO getPublicCourseAccessStatus(Long courseId);
 }
