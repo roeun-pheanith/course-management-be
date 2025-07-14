5fbd9fbd993438ead43f3bac99cda4d7fcce4b5d
unknown <pheanithroeun1@gmail.com>
2025-07-14 22:58:54 +0700

    add enrollment repository for any query method or use jpa for connect to db

--- src/main/java/com/pheanith/dev/restaurant/repository/EnrollmentRepository.java
+++ src/main/java/com/pheanith/dev/restaurant/repository/EnrollmentRepository.java
@@ -4,10 +4,9 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.pheanith.dev.restaurant.entity.Course;
 import com.pheanith.dev.restaurant.entity.Enrollment;
-import com.pheanith.dev.restaurant.entity.User;
 
 public interface EnrollmentRepository extends JpaRepository<Enrollment, Long>{
 	Optional<Enrollment> findByUserIdAndCourseId(Long userId, Long courseId);
+	boolean existsByCourseIdAndUserId(Long courseId, Long userId);
 }
