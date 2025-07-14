66ea83fe07249e007a423f77c0d89757757530cb
unknown <pheanithroeun1@gmail.com>
2025-07-14 23:13:05 +0700

    add this dto class to get access permission

---
+++ src/main/java/com/pheanith/dev/restaurant/dto/CourseAccessStatusDTO.java
@@ -0,0 +1,12 @@
+package com.pheanith.dev.restaurant.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CourseAccessStatusDTO {
+	private boolean hasAccess;
+}
